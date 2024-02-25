93c9f431d5a6fc9808c90db18f26a55b1747a86d
Priyadharshan C <88581401+Priyadharshan2003@users.noreply.github.com>
2024-02-25 13:12:45 +0530

    update
    changes

---
+++ src/components/About/About.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImg.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+              In the dynamic world of innovation and technology, I am Priyadharshan Chandranath from Salem, Tamil Nadu, India. Passionate about technology, I currently serve as the President at the Build Club in IIT Madras Research Park. Leading a team, we drive solutions to real-world challenges, exemplified by our recent success in creating a smart mirror-based home automation system.
+Complementing my academic journey, I've undertaken virtual programs with Deloitte, JPMorgan Chase, and PALS, honing skills in full-stack roles. This journey reflects my dedication, leadership, and a relentless pursuit of knowledge in the ever-evolving landscape of technology, eagerly anticipating the next chapter of this extraordinary adventure
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};

43db9772887e508648f70231197f5c4cc1d28b03
priyadharshan2003 <priyan2003chandru@gmail.com>
2024-02-25 18:06:06 +0530

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
-import { Works } from "./components/works/works";
+import { works } from "./components/works/works";
 
 function App() {
   return (
